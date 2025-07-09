73e589ff6cbf196c03613efa48a9449f9af52195
unknown <victormatias_tolosa@hotmail.com.ar>
2025-07-08 21:15:11 -0300

    paso1Catalogo

---
+++ Dominio/Catalogo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dominio
+{
+     public class Catalogo
+    {
+        public int Id { get; set; }
+        public string Codigo  { get; set; }
+        public  string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string  ImagenUrl { get; set; }
+        
+        public Categorias Inventario  { get; set; }
+        public Marcas Firmas { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
---
+++ Negocio/ConexionCatalogo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;// conexion sql
+
+namespace Negocio
+{
+    public class ConexionCatalogo
+    {
+        public List<Catalogo> listar()
+     {
+         List<Catalogo> lista = new List<Catalogo>();
+         SqlConnection conexion = new SqlConnection();
+         SqlCommand comando = new SqlCommand();
+         SqlDataReader lector;
+
+        try 
+	  {	        
+		 conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=True";  
+         comando.CommandType = System.Data.CommandType.Text;
+         comando.CommandText= "select A.Id,Codigo, Nombre, A.Descripcion, ImagenUrl, Precio, C.Descripcion Inventario,M.Descripcion Firmas from ARTICULOS A, CATEGORIAS C, MARCAS M where C.Id= A.IdCategoria and M.Id = A.IdMarca";
+         comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while(lector.Read())
+                {
+                    Catalogo aux = new Catalogo();
+                    aux.Id = (int)lector["Id"];
+                    aux.Codigo = (string)lector["Codigo"];
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    aux.ImagenUrl = (string)lector["ImagenUrl"];
+                    aux.Inventario = new Categorias();
+                    aux.Inventario.Descripcion = (string)lector["Inventario"];
+                    aux.Firmas = new Marcas();
+                    aux.Firmas.Descripcion = (string)lector["Firmas"];
+                    aux.Precio = Convert.ToDecimal(lector["Precio"]);
+
+                    lista.Add(aux);
+                }
+                conexion.Close();
+                return lista;
+          }
+	      catch (Exception ex)
+	      {
+
+		  throw;
+	      }
+
+         }
+
+
+    }
+}
